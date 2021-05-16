97d9af722fe7337fa326ab45d325411b2a8e38c2
root <root@PC.localdomain>
2021-05-16 13:42:50 +0200

    ajout joueur carteAlibi Alibiname

--- src/Modele/CarteAlibi.java
+++ src/Modele/CarteAlibi.java
@@ -5,4 +5,31 @@ Nombre de sabliers (0, 1, 2)
 Nom Personnage suspect
  */
 public class CarteAlibi {
+	
+		private AlibiName suspect;
+		private int sablier;
+
+		// Getters and Setters
+		public AlibiName getSuspect() {
+			return suspect;
+		}
+
+		public void setSuspect(AlibiName suspect) {
+			this.suspect = suspect;
+		}
+
+		public int getSablier() {
+			return sablier;
+		}
+
+		public void setSablier(int sablier) {
+			this.sablier = sablier;
+		}
+
+		// tester toString
+		public String toString() {
+			return suspect.name();
+		}
+
+	
 }
--- src/Modele/Joueur.java
+++ src/Modele/Joueur.java
@@ -1,13 +1,104 @@
 package Modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 /*
 **Joueur**:
 Boolean isJack
 Boolean sonTour
 Boolean aGagne
+String playerName jack ou detective
 if Jack: Nom Personnage suspect
 Total sabliers
 Liste cartes alibi piochés
  */
 
 public class Joueur {
+	
+	private List<CarteAlibi> cardList = new ArrayList<>();
+	private int sablier;
+	private boolean isJack = false;
+	private boolean win = false;
+	private boolean turn = false;
+	private String name;
+
+	// Constructeur 
+    //	name: nom du joueur soit jack soit dectective
+	//  isJack
+	//sablier
+	public Player(boolean isJack, String name, int sablier,boolean win,boolean turn) {
+		setJack(isJack);
+		setName(name);
+		setSablier(sablier);
+		setWinner(win);
+		setTurn(turn);
+	}
+
+	public void ajouterCarte(CarteAlibi card) {
+		System.out.println(" ajout carte : " + card);
+		cardList.add(card);
+	}
+
+	public void supprimerCarte(CarteAlibi card) {
+		System.out.println(" suppression carte : " + card);
+		cardList.remove(card);
+	}
+
+	// Getters and Setters
+	public List<CarteAlibi> getCardList() {
+		return cardList;
+	}
+
+	public void setCardList(List<CarteAlibi> CardList) {
+		this.CardList = CardList;
+	}
+
+	public int getSablier() {
+		return sablier;
+	}
+
+	public void setSablier(int sablier) {
+		this.sablier = sablier;
+	}
+
+	public boolean isJack() {
+		return isJack;
+	}
+
+	public void setJack(boolean isJack) {
+		this.isJack = isJack;
+	}
+	public boolean getWinner() {
+		return win;
+	}
+
+	public void setWinner(boolean win) {
+		this.win = win;
+	}
+    
+	public boolean getTurn() {
+		return turn;
+	}
+
+	public void setTurn(boolean turn) {
+		this.turn = turn;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Console toString
+	public String toString() {
+		return getName();
+	}
+
+	
 }
