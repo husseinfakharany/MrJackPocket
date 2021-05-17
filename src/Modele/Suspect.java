b26ac7f58378695151d25837c737940428d1984d
root <root@PC.localdomain>
2021-05-17 10:16:37 +0200

    AJOUT getter et setter suspect.java

--- src/Modele/Suspect.java
+++ src/Modele/Suspect.java
@@ -9,16 +9,51 @@ Position
 Boolean caché ou pas
  */
 public class Suspect {
-    public SuspectNom nomPersonnage;
-    public Point position; //position sur la grille
-    public Boolean carteCache;
-    public Boolean pioche;
+	
+    private SuspectNom nomPersonnage;
+    private Point position; //position sur la grille
+    private Boolean carteCache;
+    private Boolean pioche;
 
     //TODO implement getters and setters
     Suspect(SuspectNom nomPersonnage, Point position){
         this.nomPersonnage = nomPersonnage;
-        this.position = position;
-        carteCache = false;
-        pioche = false;
+        this.setPosition(position);
+        setCarteCache(false);
+        setPioche(false);
     }
+    
+   public SuspectNom getNomPersonnage(){
+    	return nomPersonnage;
+    }
+   
+   public void setNomPersonnage(SuspectNom nom ) {
+	   nomPersonnage = nom;
+   }
+
+	public Point getPosition() {
+		return position;
+	}
+	
+	public void setPosition(Point position) {
+		this.position = position;
+	}
+	
+	public Boolean getCarteCache() {
+		return carteCache;
+	}
+	
+	public void setCarteCache(Boolean carteCache) {
+		this.carteCache = carteCache;
+	}
+	
+	public Boolean getPioche() {
+		return pioche;
+	}
+	
+	public void setPioche(Boolean pioche) {
+		this.pioche = pioche;
+	}
+   
+   
 }
