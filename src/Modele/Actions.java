1dbd492eae43a5e10f0802b8d77470dc19d18936
root <root@PC.localdomain>
2021-05-17 08:53:36 +0200

    push action

---
+++ src/Modele/Actions.java
@@ -0,0 +1,8 @@
+package Modele;
+
+
+public enum Actions {
+
+	MOVE_DETECTIVE, MOVE_JOKER, DRAW_CARD, ROTATE_DISTRICT, SWAP_DISTRICT;
+
+}
--- src/Modele/JetonActions.java
+++ src/Modele/JetonActions.java
@@ -7,4 +7,49 @@ Boolean recto (True) ou verso (False)
 Boolean dejaJoue
  */
 public class JetonActions {
+	
+	Actions action1; //action face 1 
+	Actions action2;  // action face 2
+	boolean estRecto = true;
+	boolean dejaJoue = false;
+	
+	
+	public JetonActions(Actions action1, Actions action2) {
+		this.action1 = action1;
+		this.action2 = action2;
+	}
+
+
+	public Actions getAction1() {
+		return action1;
+	}
+
+	public void setAction1(Actions action1) {
+		this.action1 = action1;
+	}
+
+	public Actions getAction2() {
+		return action2;
+	}
+
+	public void setAction2(Actions action2) {
+		this.action2 = action2;
+	}
+
+	public boolean estRecto() {
+		return isRecto;
+	}
+
+	public void setEstRecto(boolean estRecto) {
+		this.estRecto = estRecto;
+	}
+
+	public boolean dejaJoue() {
+		return dejaJoue;
+	}
+
+	public void dejaJoue(boolean dejaJoue) {
+		this.dejaJoue = dejaJoue;
+	}
+	
 }
