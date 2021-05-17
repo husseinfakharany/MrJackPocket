2f63f9229624c91e9c2cbffcf44ab27ac324816e
root <root@PC.localdomain>
2021-05-17 10:56:55 +0200

    maj TestModel

--- src/TestModele.java
+++ src/TestModele.java
@@ -13,11 +13,11 @@ public class TestModele {
                 System.out.println("Carte " + compteurCarte);
                 System.out.print("Orientation: " + jeu.grille[i][j].orientation);
                 System.out.print(" - Position: " + jeu.grille[i][j].position);
-                System.out.print(" - Position personnage: " + jeu.grille[i][j].suspect.position);
+                System.out.print(" - Position personnage: " + jeu.grille[i][j].suspect.getPosition());
                 System.out.print(" - Position Enqueteur: " + jeu.grille[i][j].positionEnqueteur);
-                System.out.print(" - Nom Personnage: " + jeu.grille[i][j].suspect.nomPersonnage);
-                System.out.print(" - Carte Cachée: " + jeu.grille[i][j].suspect.carteCache);
-                System.out.print(" - Personnage pioché: " + jeu.grille[i][j].suspect.pioche);
+                System.out.print(" - Nom Personnage: " + jeu.grille[i][j].suspect.getNomPersonnage());
+                System.out.print(" - Carte Cachée: " + jeu.grille[i][j].suspect.getCarteCache());
+                System.out.print(" - Personnage pioché: " + jeu.grille[i][j].suspect.getPioche());
                 System.out.println();
                 compteurCarte++;
             }
