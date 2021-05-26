62cf59a16b800fb3d69ed8270546fae604e572b4
diamame <diamame>
2021-05-26 11:20:03 +0200

    maj iaAleatoire

--- src/Controle/IaAleatoire.java
+++ src/Controle/IaAleatoire.java
@@ -1,11 +1,20 @@
 package Controle;
 
-import Modele.Coup;
-import Modele.Jeu;
+import Modele.*;
+
 
 public class IaAleatoire extends IA{
     @Override
     Coup coupIA(Jeu j) {
+
+        Joueur joueurCourant = j.plateau().joueurCourant;
+
+        if(joueurCourant.isJack()){
+            //traitement Jack
+        }else{
+            //Traitement Enqueteur
+        }
+
         return null;
     }
 }
