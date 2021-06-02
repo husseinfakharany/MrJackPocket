36500aaee24bb5ec9a31d1ddc2dcad75566e326d
Althyia <75064985+Althyia@users.noreply.github.com>
2021-06-02 22:48:20 +0200

    Sauvegarde du nom des cartes piochées

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -146,6 +146,7 @@ public class Jeu extends Observable implements Cloneable{
                                 flux.write(entier.toString());
                                 break;
                             case INNOCENTER_CARD:
+                                flux.write(ac.getCartePioche().getSuspect().getNomPersonnage().toString());
                                 break;
                             case ROTATION_DISTRICT:
                                 entier = ac.getPosition1().y;
